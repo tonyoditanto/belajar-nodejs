e6c6dc6451ecdcf35f1bdece84f6e63ce6f8d9e3
Tony Varian Yoditanto <tonyoditanto@gmail.com>
2020-10-10 04:29:41 +0700

    install modul pdf-extraction

--- baca_pdf.js
+++ baca_pdf.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 const pdf = require("pdf-extraction");
  
-let dataBuffer = fs.readFileSync("./test.pdf");
+let dataBuffer = fs.readFileSync("./tony.pdf");
  
 pdf(dataBuffer).then(function (data) {
     console.log(data.text);
